7b6ff941e9864ed06d4fc1c08e5f18f798de1fde
Günther Noack <gnoack@google.com>
2023-11-24 18:30:21 +0100

    landlock: Add IOCTL access right
    Introduces the LANDLOCK_ACCESS_FS_IOCTL access right
    and increments the Landlock ABI version to 5.
    
    Like the truncate right, these rights are associated with a file
    descriptor at the time of open(2), and get respected even when the
    file descriptor is used outside of the thread which it was originally
    opened in.
    
    A newly enabled Landlock policy therefore does not apply to file
    descriptors which are already open.
    
    If the LANDLOCK_ACCESS_FS_IOCTL right is handled, only a small number
    of safe IOCTL commands will be permitted on newly opened files.  The
    permitted IOCTLs can be configured through the ruleset in limited ways
    now.  (See documentation for details.)
    
    Specifically, when LANDLOCK_ACCESS_FS_IOCTL is handled, granting this
    right on a file or directory will *not* permit to do all IOCTL
    commands, but only influence the IOCTL commands which are not already
    handled through other access rights.  The intent is to keep the groups
    of IOCTL commands more fine-grained.
    
    Noteworthy scenarios which require special attention:
    
    TTY devices are often passed into a process from the parent process,
    and so a newly enabled Landlock policy does not retroactively apply to
    them automatically.  In the past, TTY devices have often supported
    IOCTL commands like TIOCSTI and some TIOCLINUX subcommands, which were
    letting callers control the TTY input buffer (and simulate
    keypresses).  This should be restricted to CAP_SYS_ADMIN programs on
    modern kernels though.
    
    Some legitimate file system features, like setting up fscrypt, are
    exposed as IOCTL commands on regular files and directories -- users of
    Landlock are advised to double check that the sandboxed process does
    not need to invoke these IOCTLs.
    
    Known limitations:
    
    The LANDLOCK_ACCESS_FS_IOCTL access right is a coarse-grained control
    over IOCTL commands.  Future work will enable a more fine-grained
    access control for IOCTLs.
    
    In the meantime, Landlock users may use path-based restrictions in
    combination with their knowledge about the file system layout to
    control what IOCTLs can be done.  Mounting file systems with the nodev
    option can help to distinguish regular files and devices, and give
    guarantees about the affected files, which Landlock alone can not give
    yet.
    
    Signed-off-by: Günther Noack <gnoack@google.com>

--- security/landlock/fs.c
+++ security/landlock/fs.c
@@ -7,6 +7,7 @@
  * Copyright © 2021-2022 Microsoft Corporation
  */
 
+#include <asm/ioctls.h>
 #include <kunit/test.h>
 #include <linux/atomic.h>
 #include <linux/bitops.h>
@@ -148,7 +149,8 @@ retry:
 	LANDLOCK_ACCESS_FS_EXECUTE | \
 	LANDLOCK_ACCESS_FS_WRITE_FILE | \
 	LANDLOCK_ACCESS_FS_READ_FILE | \
-	LANDLOCK_ACCESS_FS_TRUNCATE)
+	LANDLOCK_ACCESS_FS_TRUNCATE | \
+	LANDLOCK_ACCESS_FS_IOCTL_DEV)
 /* clang-format on */
 
 /*
@@ -1332,8 +1334,10 @@ static int hook_file_alloc_security(struct file *const file)
 static int hook_file_open(struct file *const file)
 {
 	layer_mask_t layer_masks[LANDLOCK_NUM_ACCESS_FS] = {};
-	access_mask_t open_access_request, full_access_request, allowed_access;
-	const access_mask_t optional_access = LANDLOCK_ACCESS_FS_TRUNCATE;
+	access_mask_t open_access_request, full_access_request, allowed_access,
+		optional_access;
+	const struct inode *inode = file_inode(file);
+	const bool is_device = S_ISBLK(inode->i_mode) || S_ISCHR(inode->i_mode);
 	const struct landlock_ruleset *const dom = get_current_fs_domain();
 
 	if (!dom)
@@ -1350,6 +1354,10 @@ static int hook_file_open(struct file *const file)
 	 * We look up more access than what we immediately need for open(), so
 	 * that we can later authorize operations on opened files.
 	 */
+	optional_access = LANDLOCK_ACCESS_FS_TRUNCATE;
+	if (is_device)
+		optional_access |= LANDLOCK_ACCESS_FS_IOCTL_DEV;
+
 	full_access_request = open_access_request | optional_access;
 
 	if (is_access_to_paths_allowed(
@@ -1406,6 +1414,39 @@ static int hook_file_truncate(struct file *const file)
 	return -EACCES;
 }
 
+/*
+ * Returns true if the given IOCTL command is safe to do,
+ * even when it is invoked on a character or block device file.
+ */
+static int is_safe_ioctl_on_devices(unsigned int cmd)
+{
+	return vfs_masked_device_ioctl(cmd);
+}
+
+static int hook_file_ioctl(struct file *file, unsigned int cmd,
+			   unsigned long arg)
+{
+	const struct inode *inode = file_inode(file);
+	const bool is_device = S_ISBLK(inode->i_mode) || S_ISCHR(inode->i_mode);
+	access_mask_t required_access, allowed_access;
+
+	if (!is_device || is_safe_ioctl_on_devices(cmd))
+		return 0;
+
+	/*
+	 * It is the access rights at the time of opening the file which
+	 * determine whether IOCTL can be used on the opened file later.
+	 *
+	 * The access right is attached to the opened file in hook_file_open().
+	 */
+	required_access = LANDLOCK_ACCESS_FS_IOCTL_DEV;
+	allowed_access = landlock_file(file)->allowed_access;
+	if ((allowed_access & required_access) == required_access)
+		return 0;
+
+	return -EACCES;
+}
+
 static struct security_hook_list landlock_hooks[] __ro_after_init = {
 	LSM_HOOK_INIT(inode_free_security, hook_inode_free_security),
 
@@ -1428,6 +1469,7 @@ static struct security_hook_list landlock_hooks[] __ro_after_init = {
 	LSM_HOOK_INIT(file_alloc_security, hook_file_alloc_security),
 	LSM_HOOK_INIT(file_open, hook_file_open),
 	LSM_HOOK_INIT(file_truncate, hook_file_truncate),
+	LSM_HOOK_INIT(file_ioctl, hook_file_ioctl),
 };
 
 __init void landlock_add_fs_hooks(void)
