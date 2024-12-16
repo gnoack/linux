3da91029155f6a7a0620a0cb320017627e5452b7
Günther Noack <gnoack@google.com>
2024-12-16 15:18:29 +0100

    tty: Permit some TIOCL_SETSEL modes without CAP_SYS_ADMIN
    With this, processes without CAP_SYS_ADMIN are able to use TIOCLINUX with
    subcode TIOCL_SETSEL, in the selection modes TIOCL_SETPOINTER,
    TIOCL_SELCLEAR and TIOCL_SELMOUSEREPORT.
    
    TIOCL_SETSEL was previously changed to require CAP_SYS_ADMIN, as this IOCTL
    let callers change the selection buffer and could be used to simulate
    keypresses.  These three TIOCL_SETSEL selection modes, however, are safe to
    use, as they do not modify the selection buffer.
    
    This fixes a mouse support regression that affected Emacs (invisible mouse
    cursor).
    
    Link: https://lore.kernel.org/r/ee3ec63269b43b34e1c90dd8c9743bf8@finder.org
    Fixes: 8d1b43f6a6df ("tty: Restrict access to TIOCLINUX' copy-and-paste subcommands")
    Signed-off-by: Günther Noack <gnoack@google.com>

--- drivers/tty/vt/vt.c
+++ drivers/tty/vt/vt.c
@@ -3345,8 +3345,7 @@ int tioclinux(struct tty_struct *tty, unsigned long arg)
 
 	switch (type) {
 	case TIOCL_SETSEL:
-		if (!capable(CAP_SYS_ADMIN))
-			return -EPERM;
+		/* CAP_SYS_ADMIN check happens in set_selection_user(). */
 		return set_selection_user(param, tty);
 	case TIOCL_PASTESEL:
 		if (!capable(CAP_SYS_ADMIN))
